531802ca0c5b00760e01ddee54c3f6329f2667f5
Jing Zhang <289057542@qq.com>
2020-09-09 23:47:54 -0500

    Codes Adapted

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const imageUpload = document.getElementById('imageUpload')
 
 Promise.all([
   faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+  faceapi.nets.ageGenderNet.loadFromUri('/models'),
   faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
   faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
 ]).then(start)
@@ -10,8 +11,8 @@ async function start() {
   const container = document.createElement('div')
   container.style.position = 'relative'
   document.body.append(container)
-  const labeledFaceDescriptors = await loadLabeledImages()
-  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6)
+  //const labeledFaceDescriptors = await loadLabeledImages()
+  //const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6)
   let image
   let canvas
   document.body.append('Loaded')
@@ -24,12 +25,12 @@ async function start() {
     container.append(canvas)
     const displaySize = { width: image.width, height: image.height }
     faceapi.matchDimensions(canvas, displaySize)
-    const detections = await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceDescriptors()
+    const detections = await faceapi.detectAllFaces(image).withFaceLandmarks().withAgeAndGender()
     const resizedDetections = faceapi.resizeResults(detections, displaySize)
-    const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor))
-    results.forEach((result, i) => {
+    //const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor))
+    resizedDetections.forEach((result, i) => {
       const box = resizedDetections[i].detection.box
-      const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() })
+      const drawBox = new faceapi.draw.DrawBox(box, { label: Math.round(result.age) + " year old " + result.gender })
       drawBox.draw(canvas)
     })
   })
@@ -42,7 +43,7 @@ function loadLabeledImages() {
       const descriptions = []
       for (let i = 1; i <= 2; i++) {
         const img = await faceapi.fetchImage(`https://raw.githubusercontent.com/WebDevSimplified/Face-Recognition-JavaScript/master/labeled_images/${label}/${i}.jpg`)
-        const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
+        const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withAgeAndGender()
         descriptions.push(detections.descriptor)
       }
 
